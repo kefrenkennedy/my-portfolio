23e671962e55047d1654861cf89d17dc005ae1b4
Kefren <kefrenkennedyav@gmail.com>
2022-12-04 01:40:19 -0300

    clone: kenzie's repo

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Kefren Vasconcelos",
+  githubUser: "kefrenkennedy",
+  linkedinUser: "kefren-vasconcelos",
+  whatsappNumber: "+5588981968722",
+  emailUser: "kefrenkennedyav@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
