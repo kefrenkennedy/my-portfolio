cb112d1879837d3abbcbc529326ffc944c942c60
Kefren <kefrenkennedyav@gmail.com>
2023-02-26 22:14:19 -0300

    initial commit

---
+++ src/utils/stackData.ts
@@ -0,0 +1,122 @@
+import imgHTML from "../public/static/img/stack/html.svg";
+import imgCSS from "../public/static/img/stack/css.svg";
+import imgJS from "../public/static/img/stack/js.svg";
+import imgNode from "../public/static/img/stack/node.svg";
+import imgStyled from "../public/static/img/stack/styled.svg";
+import imgReact from "../public/static/img/stack/react.svg";
+import imgBootstrap from "../public/static/img/stack/bootstrap.svg";
+import imgDjango from "../public/static/img/stack/django.svg";
+import imgExpress from "../public/static/img/stack/express.svg";
+import imgGit from "../public/static/img/stack/git.svg";
+import imgJira from "../public/static/img/stack/jira.svg";
+import imgJwt from "../public/static/img/stack/jwt.svg";
+import imgKanban from "../public/static/img/stack/kanban.svg";
+import imgPostgres from "../public/static/img/stack/postgresql.svg";
+import imgPrisma from "../public/static/img/stack/prisma.svg";
+import imgPython from "../public/static/img/stack/python.svg";
+import imgRedux from "../public/static/img/stack/redux.svg";
+import imgRegex from "../public/static/img/stack/regex.svg";
+import imgScrum from "../public/static/img/stack/scrum.svg";
+import imgSlack from "../public/static/img/stack/slack.svg";
+import imgSql from "../public/static/img/stack/sql.svg";
+import imgTrello from "../public/static/img/stack/trello.svg";
+import imgTypeORM from "../public/static/img/stack/typeORM.svg";
+
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "Git",
+    img: imgGit,
+  },
+  { title: "Docker", img: FaDocker },
+  {
+    title: "Python",
+    img: imgPython,
+  },
+  {
+    title: "Django",
+    img: imgDjango,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  {
+    title: "JS",
+    img: imgJS,
+  },
+  {
+    title: "HTML",
+    img: imgHTML,
+  },
+  {
+    title: "CSS",
+    img: imgCSS,
+  },
+  {
+    title: "React",
+    img: imgReact,
+  },
+  {
+    title: "Node",
+    img: imgNode,
+  },
+  {
+    title: "Bootstrap",
+    img: imgBootstrap,
+  },
+  {
+    title: "Redux",
+    img: imgRedux,
+  },
+
+  {
+    title: "Express",
+    img: imgExpress,
+  },
+  {
+    title: "SQL",
+    img: imgSql,
+  },
+  {
+    title: "PostgreSQL",
+    img: imgPostgres,
+  },
+  {
+    title: "TypeORM",
+    img: imgTypeORM,
+  },
+  {
+    title: "Prisma",
+    img: imgPrisma,
+  },
+  {
+    title: "JSON Web Token",
+    img: imgJwt,
+  },
+
+  {
+    title: "Slack",
+    img: imgSlack,
+  },
+  {
+    title: "Scrum",
+    img: imgScrum,
+  },
+  {
+    title: "Jira",
+    img: imgJira,
+  },
+  {
+    title: "Kanban",
+    img: imgKanban,
+  },
+  {
+    title: "Trello",
+    img: imgTrello,
+  },
+
+  {
+    title: "Regex",
+    img: imgRegex,
+  },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+  nameUser: "Kefren Vasconcelos",
+  githubUser: "kefrenkennedy",
+  linkedinUser: "kefren-vasconcelos",
+  whatsappNumber: "+5588981968722",
+};
