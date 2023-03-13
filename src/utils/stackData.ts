25617a3168b031b4b75103fb1be800b7807b3f57
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-03-13 19:23:57 -0300

    addeded more stacks

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,5 +1,17 @@
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
+import { TbBrandBootstrap } from "react-icons/tb";
+import {
+  SiPostgresql,
+  SiJirasoftware,
+  SiTypescript,
+  SiJsonwebtokens,
+  SiRedux,
+  SiExpress,
+  SiPrisma,
+  SiStyledcomponents,
+  SiSqlite,
+  SiJest,
+  SiNotion,
+} from "react-icons/si";
 import { FaGit, FaPython } from "react-icons/fa";
 import {
   FaDocker,
@@ -7,22 +19,18 @@ import {
   FaCss3Alt,
   FaJs,
   FaNode,
+  FaDatabase, //SQL
   FaReact,
+  FaSlack,
 } from "react-icons/fa";
 
+import { BsKanbanFill } from "react-icons/bs";
+
+import { BiCodeCurly } from "react-icons/bi"; //typeORM
+
+import { DiDjango, DiScrum, DiTrello } from "react-icons/di";
+
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
   { title: "TypeScript", img: SiTypescript },
   {
     title: "Node JS",
@@ -32,8 +40,89 @@ export const stackData = [
     title: "React",
     img: FaReact,
   },
-  { title: "Docker", img: FaDocker },
+  {
+    title: "JavaScript",
+    img: FaJs,
+  },
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "SQL",
+    img: FaDatabase,
+  },
   { title: "PostgreSQL", img: SiPostgresql },
-  { title: "Git", img: FaGit },
+  {
+    title: "TypeORM",
+    img: BiCodeCurly,
+  },
+  {
+    title: "Prisma",
+    img: SiPrisma,
+  },
+  {
+    title: "Express",
+    img: SiExpress,
+  },
+  {
+    title: "Styled-Components",
+    img: SiStyledcomponents,
+  },
+  {
+    title: "SQLite",
+    img: SiSqlite,
+  },
   { title: "Python", img: FaPython },
+  {
+    title: "Django",
+    img: DiDjango,
+  },
+  {
+    title: "Jest",
+    img: SiJest,
+  },
+  { title: "Docker", img: FaDocker },
+  { title: "Git", img: FaGit },
+
+  {
+    title: "Redux",
+    img: SiRedux,
+  },
+  {
+    title: "JSON Web Token",
+    img: SiJsonwebtokens,
+  },
+  {
+    title: "Bootstrap",
+    img: TbBrandBootstrap,
+  },
+  {
+    title: "Trello",
+    img: DiTrello,
+  },
+  {
+    title: "Slack",
+    img: FaSlack,
+  },
+  {
+    title: "Scrum",
+    img: DiScrum,
+  },
+  {
+    title: "Jira",
+    img: SiJirasoftware,
+  },
+  {
+    title: "Kanban",
+    img: BsKanbanFill,
+  },
+  {
+    title: "Notion",
+    img: SiNotion,
+  },
 ];
