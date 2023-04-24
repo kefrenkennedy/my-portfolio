d936f095ec66f5f45e86d4965d441f990f8491aa
kefrenkennedy <kefrenkennedyav@gmail.com>
2023-04-24 13:13:06 -0300

    Update nameUser to "Kefren Vasconcelos" in userData.ts

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,5 +1,5 @@
 export const userData = {
-  nameUser: "Kefren Kennedy",
+  nameUser: "Kefren Vasconcelos",
   githubUser: "kefrenkennedy",
   linkedinUser: "kefren-vasconcelos",
   whatsappNumber: "+5588981968722",
